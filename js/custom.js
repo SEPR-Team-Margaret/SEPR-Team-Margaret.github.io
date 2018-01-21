0b3476b8ba795efad4cd793db97b855988408ede
David Clarke <dclarkeu@gmail.com>
2018-01-21 16:45:21 +0000

    First Commit of Site 2.0

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    // Initialize collapse button
+	$(".button-collapse").sideNav();
+	// Initialize dropdown button
+	$(".dropdown-button").dropdown();
+	// Initialize collapsible
+	$('.collapsible').collapsible();
+});
